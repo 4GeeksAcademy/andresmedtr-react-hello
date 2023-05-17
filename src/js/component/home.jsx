51ca21455896392c450ac94e9818d766f25f039b
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-05-17 23:16:37 +0000

    Finished project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,36 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	let firstSecond = Math.floor(props.counter / 1)% 10
+	let secondSecond =  Math.floor(props.counter / 10)% 10
+	let thirdSecond =  Math.floor(props.counter / 100)% 10 
+	let fourthSecond =  Math.floor(props.counter / 1000)% 10
+	let fifthSecond =  Math.floor(props.counter / 10000)% 10
+	let sixthSecond =  Math.floor(props.counter / 100000)% 10
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bg-black text-center d-flex justify-content-center text-white mx-3">
+			<div className="bg-dark clock align-items-center d-flex bg-light-subtle">
+				<i class="fa-regular fa-clock mx-auto "></i>
+			</div>
+			<div className='digit bg-dark'>
+				{sixthSecond}
+			</div>
+			<div className='digit bg-dark'>
+				{fifthSecond}
+			</div>
+			<div className='digit bg-dark'>
+				{fourthSecond}
+			</div>
+			<div className='digit bg-dark'>
+				{thirdSecond}
+			</div>
+			<div className='digit bg-dark'>
+				{secondSecond}
+			</div>
+			<div className='digit bg-dark'>
+				{firstSecond}
+			</div>
 		</div>
 	);
 };
